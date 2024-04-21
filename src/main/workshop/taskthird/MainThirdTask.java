907399876d035e9653a20c9520740ee6673ee1a3
destructiv-e <tolstok.sav@mail.ru>
2024-04-21 21:15:58 +0300

    second task

--- src/main/workshop/taskthird/MainThirdTask.java
+++ src/main/workshop/taskthird/MainThirdTask.java
@@ -11,8 +11,8 @@ public class MainThirdTask {
         {-1.48213, -0.05316, 1.08254},
         {-0.05316, 1.13958, 0.01617},
         {1.08254, 0.01617, -1.48271}});
-    List<Integer> res = matrixA.findIkJk(matrixA.getMatrix());
-    System.out.println(res.get(0));
-    System.out.println(res.get(1));
+    //List<Integer> res = matrixA.findIkJk(matrixA.getMatrix());
+    //System.out.println(res.get(0));
+    //System.out.println(res.get(1));
   }
 }
